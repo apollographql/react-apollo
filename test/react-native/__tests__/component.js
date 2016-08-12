8bba4ff86c6a3f71dab662c4822f7058e8f8cb94
James Baxley <james.baxley@newspring.cc>
2016-08-11 23:52:18 -0400

    WORKING REACT NATIVE TEST :rocket: :rocket: :rocket:

--- test/react-native/__tests__/component.js
+++ test/react-native/__tests__/component.js
@@ -34,31 +34,30 @@ describe('App', () => {
     expect(output.toJSON()).toMatchSnapshot();
   });
 
-  // XXX there appears to be a bug in testing apollo-client
-  // with jest and react-native. Need to debug more
-  // it('executes a query', (done) => {
-  //   const query = gql`query people { allPeople(first: 1) { people { name } } }`;
-  //   const data = { allPeople: { people: { name: 'Luke Skywalker' } } };
-  //   const networkInterface = mockNetworkInterface({ request: { query }, result: { data } });
-  //   const client = new ApolloClient({ networkInterface });
+  it('executes a query', (done) => {
+    jest.useRealTimers()
+    const query = gql`query people { allPeople(first: 1) { people { name } } }`;
+    const data = { allPeople: { people: { name: 'Luke Skywalker' } } };
+    const networkInterface = mockNetworkInterface({ request: { query }, result: { data } });
+    const client = new ApolloClient({ networkInterface });
 
-  //   class Container extends Component {
-  //     componentWillReceiveProps(props) {
-  //       expect(props.data.loading).toBeFalsy();
-  //       expect(props.data.allPeople.people.name).toEqual(data.allPeople.people.name);
-  //       done();
-  //     }
-  //     render() {
-  //       if (this.props.data.loading) return <Text>Loading...</Text>
-  //       return <Text>{this.props.data.allPeople.people.name}</Text>;
-  //     }
-  //   };
+    class Container extends Component {
+      componentWillReceiveProps(props) {
+        expect(props.data.loading).toBeFalsy();
+        expect(props.data.allPeople.people.name).toEqual(data.allPeople.people.name);
+        done();
+      }
+      render() {
+        if (this.props.data.loading) return <Text>Loading...</Text>
+        return <Text>{this.props.data.allPeople.people.name}</Text>;
+      }
+    };
 
-  //   const ContainerWithData = graphql(query)(Container);
+    const ContainerWithData = graphql(query)(Container);
 
-  //   const output = renderer.create(
-  //     <ApolloProvider client={client}><ContainerWithData /></ApolloProvider>
-  //   )
-  // });
+    const output = renderer.create(
+      <ApolloProvider client={client}><ContainerWithData /></ApolloProvider>
+    )
+  });
 
 });
