c614d314d8e6b951d47e64eda98fe9e594b598f0
James Baxley <james.baxley@newspring.cc>
2016-08-11 23:03:06 -0400

    make test more isolated

--- test/react-native/__tests__/component.js
+++ test/react-native/__tests__/component.js
@@ -1,18 +1,41 @@
 // __tests__/Intro-test.js
 import 'react-native';
 import React from 'react';
-import App from '../component';
+import Component from '../component';
+// import { mount, shallow } from 'enzyme';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+import ApolloClient from 'apollo-client';
+import gql from 'graphql-tag';
+import { ApolloProvider, graphql } from '../../../lib/src';
+import mockNetworkInterface from '../../../lib/test/mocks/mockNetworkInterface';
+
 describe('App', () => {
 
   it('renders correctly', () => {
+    const query = gql`query people { allPeople(first: 1) { people { name } } }`;
+    const data = { allPeople: { people: { name: 'Luke Skywalker' } } };
+    const networkInterface = mockNetworkInterface({ request: { query }, result: { data } });
+    const client = new ApolloClient({ networkInterface });
+
     const tree = renderer.create(
-      <App />
+      <ApolloProvider client={client}><Component /></ApolloProvider>
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  // it('renders correctly with data', () => {
+  //   const ContainerWithData =  graphql(query)(({ data }) => { // tslint:disable-line
+  //     expect(data).to.exist;
+  //     expect(data.ownProps).to.not.exist;
+  //     expect(data.loading).to.be.true;
+  //     return null;
+  //   });
+
+  //   const wrapper = mount(<ApolloProvider client={client}><ContainerWithData /></ApolloProvider>);
+  //   wrapper.unmount();
+  // });
 
 });
--- test/react-native/component.js
+++ test/react-native/component.js
@@ -9,27 +9,12 @@ import {
 // react native doesn't support jsx so typescript isn't useful yet
 // there is a PR in typescript to allow jsx to convert to js which
 // would allow us to use the src files again
-import ApolloClient from 'apollo-client';
 import gql from 'graphql-tag';
 import { ApolloProvider, graphql } from '../../lib/src';
-import mockNetworkInterface from '../../lib/test/mocks/mockNetworkInterface';
 
-export const query = gql`query people { allPeople(first: 1) { people { name } } }`;
-export const data = { allPeople: { people: { name: 'Luke Skywalker' } } };
-export const networkInterface = mockNetworkInterface({ request: { query }, result: { data } });
-export const client = new ApolloClient({ networkInterface });
+const query = gql`query people { allPeople(first: 1) { people { name } } }`;
 
-export const ContainerWithData =  graphql(query)(({ data }) => { // tslint:disable-line
+export default graphql(query)(({ data }) => {
   if (data.loading) return <Text>Loading...</Text>
   return <Text>{data.allPeople.people.name}</Text>;
 });
-
-export default class App extends Component {
-  render() {
-    return (
-      <ApolloProvider client={client} >
-        <ContainerWithData />
-      </ApolloProvider>
-    );
-  }
-}
