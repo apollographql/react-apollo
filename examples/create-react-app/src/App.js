ca683422e708119964e15f3f417a1e146af91e10
James Baxley <james.baxley@newspring.cc>
2016-10-31 01:01:24 -0400

    add a few sample projects and locally fix failing test env

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from "../../../lib/src";
 
-import Articles from "./Articles";
+import Pokemon from "./Pokemon";
 
-export const networkInterface = createNetworkInterface({ uri: '/graphql' });
+export const networkInterface = createNetworkInterface({ uri: 'https://graphql-pokemon.now.sh/' });
 export const client = new ApolloClient({ networkInterface });
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <Articles />
+        <Pokemon />
       </ApolloProvider>
     );
   }
