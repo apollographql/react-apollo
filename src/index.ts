01478590c59e3fd6a30d924ed42fe84ee94f0bf2
shian15810 <shian15810@gmail.com>
2017-04-12 08:54:39 +0800

    exposed createBatchingNetworkInterface from apollo-client

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from './browser';
 
 export { getDataFromTree, renderToStringWithData } from './server';
 
-export { default as ApolloClient, createNetworkInterface } from 'apollo-client';
+export { default as ApolloClient, createNetworkInterface, createBatchingNetworkInterface } from 'apollo-client';
 export { default as gql } from 'graphql-tag'
