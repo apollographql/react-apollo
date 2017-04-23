627d7524eb9925c974d35a3ddc99a9c212a0679a
Ben James <benhjames@sky.com>
2017-04-23 23:42:38 +0100

    Re-export everything from apollo-client

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 export * from './browser';
 
 export { getDataFromTree, renderToStringWithData } from './server';
-
-export { default as ApolloClient, createNetworkInterface, createBatchingNetworkInterface } from 'apollo-client';
-export { default as gql } from 'graphql-tag';
