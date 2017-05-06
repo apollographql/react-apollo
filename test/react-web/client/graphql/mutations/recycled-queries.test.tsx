64ddd42a1d0a26d52e6e764ea80cdb34f8d953a8
James Baxley <james.baxley@newspring.cc>
2017-05-05 22:18:20 -0400

    Revert "Revert "make testing more accessible with smaller files"" (#685)
    * Revert "Revert "make testing more accessible with smaller files (#682)" (#684)"
    
    This reverts commit dc077243ca86c715081279c9e136f433dfdd8c22.
    
    * update 1.2.1 of apollo-client
    
    * remove uneccessary expect statement

---
+++ test/react-web/client/graphql/mutations/recycled-queries.test.tsx
@@ -0,0 +1,387 @@
+
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import gql from 'graphql-tag';
+import assign = require('object-assign');
+
+import ApolloClient from 'apollo-client';
+
+declare function require(name: string);
+
+import { mockNetworkInterface } from '../../../../../src/test-utils';
+
+
+import { ApolloProvider, graphql } from '../../../../../src';
+
+describe('[mutations] update queries', () => {
+  // This is a long test that keeps track of a lot of stuff. It is testing
+  // whether or not the `updateQueries` reducers will run even when a given
+  // container component is unmounted.
+  //
+  // It does this with the following procedure:
+  //
+  // 1. Mount a mutation component.
+  // 2. Mount a query component.
+  // 3. Run the mutation in the mutation component.
+  // 4. Check the props in the query component.
+  // 5. Unmount the query component.
+  // 6. Run the mutation in the mutation component again.
+  // 7. Remount the query component.
+  // 8. Check the props in the query component to confirm that the mutation
+  //    that was run while we were unmounted changed the query component’s
+  //    props.
+  //
+  // There are also a lot more assertions on the way to make sure everything is
+  // going as smoothly as planned.
+  it('will run `updateQueries` for a previously mounted component', () => new Promise((resolve, reject) => {
+    const mutation = gql`
+      mutation createTodo {
+        createTodo { id, text, completed }
+      }
+    `;
+
+    const mutationData = {
+      createTodo: {
+        id: '99',
+        text: 'This one was created with a mutation.',
+        completed: true,
+      },
+    };
+
+    let todoUpdateQueryCount = 0;
+
+    const updateQueries = {
+      todos: (previousQueryResult, { mutationResult, queryVariables }) => {
+        todoUpdateQueryCount++;
+
+        if (queryVariables.id !== '123') {
+          // this isn't the query we updated, so just return the previous result
+          return previousQueryResult;
+        }
+        // otherwise, create a new object with the same shape as the
+        // previous result with the mutationResult incorporated
+        const originalList = previousQueryResult.todo_list;
+        const newTask = mutationResult.data.createTodo;
+        return {
+          todo_list: assign(originalList, { tasks: [...originalList.tasks, newTask] }),
+        };
+      },
+    };
+
+    const query = gql`
+      query todos($id: ID!) {
+        todo_list(id: $id) {
+          id, title, tasks { id, text, completed }
+        }
+      }
+    `;
+
+    const data = {
+      todo_list: { id: '123', title: 'how to apollo', tasks: [] },
+    };
+
+    const networkInterface = mockNetworkInterface(
+      { request: { query, variables: { id: '123' } }, result: { data } },
+      { request: { query: mutation }, result: { data: mutationData } },
+      { request: { query: mutation }, result: { data: mutationData } },
+    );
+    const client = new ApolloClient({ networkInterface, addTypename: false });
+
+    let mutate;
+
+    @graphql(mutation, { options: () => ({ updateQueries }) })
+    class Mutation extends React.Component<any, any> {
+      componentDidMount () {
+        mutate = this.props.mutate;
+      }
+
+      render () {
+        return null;
+      }
+    }
+
+    let queryMountCount = 0;
+    let queryUnmountCount = 0;
+    let queryRenderCount = 0;
+
+    @graphql(query)
+    class Query extends React.Component<any, any> {
+      componentWillMount () {
+        queryMountCount++;
+      }
+
+      componentWillUnmount () {
+        queryUnmountCount++;
+      }
+
+      render () {
+        try {
+          switch (queryRenderCount++) {
+            case 0:
+              expect(this.props.data.loading).toBe(true);
+              expect(this.props.data.todo_list).toBeFalsy();
+              break;
+            case 1:
+              expect(this.props.data.todo_list).toEqual({
+                id: '123',
+                title: 'how to apollo',
+                tasks: [],
+              });
+              break;
+            case 2:
+              expect(queryMountCount).toBe(1);
+              expect(queryUnmountCount).toBe(0);
+              expect(this.props.data.todo_list).toEqual({
+                id: '123',
+                title: 'how to apollo',
+                tasks: [
+                  {
+                    id: '99',
+                    text: 'This one was created with a mutation.',
+                    completed: true,
+                  },
+                ],
+              });
+              break;
+            case 3:
+              expect(queryMountCount).toBe(2);
+              expect(queryUnmountCount).toBe(1);
+              expect(this.props.data.todo_list).toEqual({
+                id: '123',
+                title: 'how to apollo',
+                tasks: [
+                  {
+                    id: '99',
+                    text: 'This one was created with a mutation.',
+                    completed: true,
+                  },
+                  {
+                    id: '99',
+                    text: 'This one was created with a mutation.',
+                    completed: true,
+                  },
+                ],
+              });
+              break;
+            case 4:
+              expect(this.props.data.todo_list).toEqual({
+                id: '123',
+                title: 'how to apollo',
+                tasks: [
+                  {
+                    id: '99',
+                    text: 'This one was created with a mutation.',
+                    completed: true,
+                  },
+                  {
+                    id: '99',
+                    text: 'This one was created with a mutation.',
+                    completed: true,
+                  },
+                ],
+              });
+              break;
+            default:
+              throw new Error('Rendered too many times');
+          }
+        } catch (error) {
+          reject(error);
+        }
+        return null;
+      }
+    }
+
+    const wrapperMutation = renderer.create(
+      <ApolloProvider client={client}>
+        <Mutation/>
+      </ApolloProvider>
+    );
+
+    const wrapperQuery1 = renderer.create(
+      <ApolloProvider client={client}>
+        <Query id="123"/>
+      </ApolloProvider>
+    );
+
+    setTimeout(() => {
+      mutate();
+
+      setTimeout(() => {
+        try {
+          expect(queryUnmountCount).toBe(0);
+          wrapperQuery1.unmount();
+          expect(queryUnmountCount).toBe(1);
+        } catch (error) {
+          reject(error);
+          throw error;
+        }
+
+        setTimeout(() => {
+          mutate();
+
+          setTimeout(() => {
+            const wrapperQuery2 = renderer.create(
+              <ApolloProvider client={client}>
+                <Query id="123"/>
+              </ApolloProvider>
+            );
+
+            setTimeout(() => {
+              wrapperMutation.unmount();
+              wrapperQuery2.unmount();
+
+              try {
+                expect(todoUpdateQueryCount).toBe(2);
+                expect(queryMountCount).toBe(2);
+                expect(queryUnmountCount).toBe(2);
+                expect(queryRenderCount).toBe(5);
+                resolve();
+              } catch (error) {
+                reject(error);
+                throw error;
+              }
+            }, 5);
+          }, 5);
+        }, 5);
+      }, 6);
+    }, 5);
+  }));
+
+  it('will run `refetchQueries` for a recycled queries', () => new Promise((resolve, reject) => {
+    const mutation = gql`
+      mutation createTodo {
+        createTodo { id, text, completed }
+      }
+    `;
+
+    const mutationData = {
+      createTodo: {
+        id: '99',
+        text: 'This one was created with a mutation.',
+        completed: true,
+      },
+    };
+
+    const query = gql`
+      query todos($id: ID!) {
+        todo_list(id: $id) {
+          id, title, tasks { id, text, completed }
+        }
+      }
+    `;
+
+    const data = {
+      todo_list: { id: '123', title: 'how to apollo', tasks: [] },
+    };
+
+    const updatedData = {
+      todo_list: { id: '123', title: 'how to apollo', tasks: [mutationData.createTodo] },
+    };
+
+    const networkInterface = mockNetworkInterface(
+      { request: { query, variables: { id: '123' } }, result: { data } },
+      { request: { query: mutation }, result: { data: mutationData } },
+      {
+        request: { query, variables: { id: '123' } },
+        result: { data: updatedData }
+      },
+    );
+    const client = new ApolloClient({ networkInterface, addTypename: false });
+
+    let mutate;
+
+    @graphql(mutation, {})
+    class Mutation extends React.Component<any, any> {
+      componentDidMount () {
+        mutate = this.props.mutate;
+      }
+
+      render () {
+        return null;
+      }
+    }
+
+    let queryMountCount = 0;
+    let queryUnmountCount = 0;
+    let queryRenderCount = 0;
+
+    @graphql(query)
+    class Query extends React.Component<any, any> {
+      componentWillMount () {
+        queryMountCount++;
+      }
+
+      componentWillUnmount () {
+        queryUnmountCount++;
+      }
+
+      render () {
+        try {
+          switch (queryRenderCount++) {
+            case 0:
+              expect(this.props.data.loading).toBe(true);
+              expect(this.props.data.todo_list).toBeFalsy();
+              break;
+            case 1:
+              expect(this.props.data.loading).toBe(false);
+              expect(this.props.data.todo_list).toEqual({
+                id: '123',
+                title: 'how to apollo',
+                tasks: [],
+              });
+              break;
+            case 2:
+              expect(queryMountCount).toBe(2);
+              expect(queryUnmountCount).toBe(1);
+              expect(this.props.data.todo_list).toEqual(updatedData.todo_list);
+              break;
+            case 3:
+              expect(queryMountCount).toBe(2);
+              expect(queryUnmountCount).toBe(1);
+              expect(this.props.data.todo_list).toEqual(updatedData.todo_list);
+              break;
+            default:
+              throw new Error('Rendered too many times');
+          }
+        } catch (error) {
+          reject(error);
+        }
+        return null;
+      }
+    }
+
+    const wrapperMutation = renderer.create(
+      <ApolloProvider client={client}>
+        <Mutation/>
+      </ApolloProvider>
+    );
+
+    const wrapperQuery1 = renderer.create(
+      <ApolloProvider client={client}>
+        <Query id="123"/>
+      </ApolloProvider>
+    );
+
+    setTimeout(() => {
+      wrapperQuery1.unmount();
+
+      mutate({ refetchQueries: ['todos'] })
+        .then((...args) => {
+          setTimeout(() => {
+            // This re-renders the recycled query that should have been refetched while recycled.
+            const wrapperQuery2 = renderer.create(
+              <ApolloProvider client={client}>
+                <Query id="123"/>
+              </ApolloProvider>
+            );
+            resolve();
+          }, 5);
+        })
+        .catch((error) => {
+          reject(error);
+          throw error;
+        });
+    }, 5);
+  }));
+
+});
---
+++ test/react-web/client/graphql/queries/index.test.tsx
@@ -0,0 +1,362 @@
+/// <reference types="jest" />
+
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import * as ReactDOM from 'react-dom';
+import * as renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import gql from 'graphql-tag';
+import ApolloClient, { ApolloError, ObservableQuery } from 'apollo-client';
+import { NetworkInterface } from 'apollo-client';
+import { connect } from 'react-redux';
+import { withState } from 'recompose';
+
+declare function require(name: string);
+
+import { mockNetworkInterface } from '../../../../../src/test-utils';
+import { ApolloProvider, graphql} from '../../../../../src';
+
+// XXX: this is also defined in apollo-client
+// I'm not sure why mocha doesn't provide something like this, you can't
+// always use promises
+const wrap = (done: Function, cb: (...args: any[]) => any) => (...args: any[]) => {
+  try {
+    return cb(...args);
+  } catch (e) {
+    done(e);
+  }
+};
+
+function wait(ms) {
+  return new Promise(resolve => setTimeout(() => resolve(), ms));
+}
+
+describe('queries', () => {
+
+  // general api
+  it('binds a query to props', () => {
+    const query = gql`query people { allPeople(first: 1) { people { name } } }`;
+    const data = { allPeople: { people: [ { name: 'Luke Skywalker' } ] } };
+    const networkInterface = mockNetworkInterface({ request: { query }, result: { data } });
+    const client = new ApolloClient({ networkInterface, addTypename: false });
+
+    const ContainerWithData = graphql(query)(({ data }) => { // tslint:disable-line
+      expect(data).toBeTruthy();
+      expect(data.ownProps).toBeFalsy();
+      expect(data.loading).toBe(true);
+      return null;
+    });
+
+    const output = renderer.create(<ApolloProvider client={client}><ContainerWithData /></ApolloProvider>);
+    output.unmount();
+  });
+
+  it('includes the variables in the props', () => {
+    const query = gql`query people ($first: Int) { allPeople(first: $first) { people { name } } }`;
+    const data = { allPeople: { people: [ { name: 'Luke Skywalker' } ] } };
+    const variables = { first: 1 };
+    const networkInterface = mockNetworkInterface(
+      { request: { query, variables }, result: { data } }
+    );
+    const client = new ApolloClient({ networkInterface, addTypename: false });
+
+    const ContainerWithData =  graphql(query)(({ data }) => { // tslint:disable-line
+      expect(data).toBeTruthy();;
+      expect(data.variables).toEqual(variables);
+      return null;
+    });
+
+    renderer.create(<ApolloProvider client={client}><ContainerWithData first={1} /></ApolloProvider>);
+  });
+
+  it("shouldn't warn about fragments", () => {
+    const oldWarn = console.warn;
+    const warnings = [];
+    console.warn = (str) => warnings.push(str);
+
+    try {
+      graphql(gql`query foo { bar }`);
+      expect(warnings.length).toEqual(0);
+    } finally {
+      console.warn = oldWarn;
+    }
+  });
+
+  it('executes a query', (done) => {
+    const query = gql`query people { allPeople(first: 1) { people { name } } }`;
+    const data = { allPeople: { people: [ { name: 'Luke Skywalker' } ] } };
+    const networkInterface = mockNetworkInterface({ request: { query }, result: { data } });
+    const client = new ApolloClient({ networkInterface, addTypename: false });
+
+    @graphql(query)
+    class Container extends React.Component<any, any> {
+      componentWillReceiveProps(props) {
+        expect(props.data.loading).toBe(false);
+        expect(props.data.allPeople).toEqual(data.allPeople);
+        done();
+      }
+      render() {
+        return null;
+      }
+    };
+
+    renderer.create(<ApolloProvider client={client}><Container /></ApolloProvider>);
+  });
+
+  it('executes a query with two root fields', (done) => {
+    const query = gql`query people {
+      allPeople(first: 1) { people { name } }
+      otherPeople(first: 1) { people { name } }
+    }`;
+    const data = {
+      allPeople: { people: [ { name: 'Luke Skywalker' } ] },
+      otherPeople: { people: [ { name: 'Luke Skywalker' } ] },
+    };
+    const networkInterface = mockNetworkInterface({ request: { query }, result: { data } });
+    const client = new ApolloClient({ networkInterface, addTypename: false });
+
+    @graphql(query)
+    class Container extends React.Component<any, any> {
+      componentWillReceiveProps(props) {
+        expect(props.data.loading).toBe(false);
+        expect(props.data.allPeople).toEqual(data.allPeople);
+        expect(props.data.otherPeople).toEqual(data.otherPeople);
+        done();
+      }
+      render() {
+        return null;
+      }
+    };
+
+    renderer.create(<ApolloProvider client={client}><Container /></ApolloProvider>);
+  });
+
+  it('maps props as variables if they match', (done) => {
+    const query = gql`
+      query people($first: Int) {
+        allPeople(first: $first) { people { name } }
+      }
+    `;
+    const data = { allPeople: { people: [ { name: 'Luke Skywalker' } ] } };
+    const variables = { first: 1 };
+    const networkInterface = mockNetworkInterface({
+      request: { query, variables },
+      result: { data },
+    });
+    const client = new ApolloClient({ networkInterface, addTypename: false });
+
+    @graphql(query)
+    class Container extends React.Component<any, any> {
+      componentWillReceiveProps(props) {
+        expect(props.data.loading).toBe(false);
+        expect(props.data.allPeople).toEqual(data.allPeople);
+        expect(props.data.variables).toEqual(this.props.data.variables);
+        done();
+      }
+      render() {
+        return null;
+      }
+    };
+
+    renderer.create(<ApolloProvider client={client}><Container first={1} /></ApolloProvider>);
+  });
+
+  it('allows falsy values in the mapped variables from props', (done) => {
+    const query = gql`
+      query people($first: Int) {
+        allPeople(first: $first) { people { name } }
+      }
+    `;
+    const data = { allPeople: { people: [ { name: 'Luke Skywalker' } ] } };
+    const variables = { first: null };
+    const networkInterface = mockNetworkInterface({
+      request: { query, variables },
+      result: { data },
+    });
+    const client = new ApolloClient({ networkInterface, addTypename: false });
+
+    @graphql(query)
+    class Container extends React.Component<any, any> {
+      componentWillReceiveProps(props) {
+        expect(props.data.loading).toBe(false);
+        expect(props.data.allPeople).toEqual(data.allPeople);
+        done();
+      }
+      render() {
+        return null;
+      }
+    };
+
+    renderer.create(<ApolloProvider client={client}><Container first={null} /></ApolloProvider>);
+  });
+
+  it('doesn\'t error on optional required props', () => {
+    const query = gql`
+      query people($first: Int) {
+        allPeople(first: $first) { people { name } }
+      }
+    `;
+    const data = { allPeople: { people: [ { name: 'Luke Skywalker' } ] } };
+    const variables = { first: 1 };
+    const networkInterface = mockNetworkInterface({
+      request: { query, variables },
+      result: { data },
+    });
+    const client = new ApolloClient({ networkInterface, addTypename: false });
+    const Container =  graphql(query)(() => null);
+
+    let error = null;
+    try {
+      renderer.create(<ApolloProvider client={client}><Container first={1} /></ApolloProvider>);
+    } catch (e) { error = e; }
+
+    expect(error).toBeNull();
+
+  });
+
+  it('errors if the passed props don\'t contain the needed variables', () => {
+    const query = gql`
+      query people($first: Int!) {
+        allPeople(first: $first) { people { name } }
+      }
+    `;
+    const data = { allPeople: { people: [ { name: 'Luke Skywalker' } ] } };
+    const variables = { first: 1 };
+    const networkInterface = mockNetworkInterface({
+      request: { query, variables },
+      result: { data },
+    });
+    const client = new ApolloClient({ networkInterface, addTypename: false });
+    const Container =  graphql(query)(() => null);
+
+    try {
+      renderer.create(<ApolloProvider client={client}><Container frst={1} /></ApolloProvider>);
+    } catch (e) {
+      expect(e.name).toMatch(/Invariant Violation/);
+      expect(e.message).toMatch(/The operation 'people'/);
+    }
+
+  });
+
+  // context
+  it('allows context through updates', (done) => {
+    const query = gql`query people { allPeople(first: 1) { people { name } } }`;
+    const data = { allPeople: { people: [ { name: 'Luke Skywalker' } ] } };
+    const networkInterface = mockNetworkInterface({ request: { query }, result: { data } });
+    const client = new ApolloClient({ networkInterface, addTypename: false });
+
+    @graphql(query)
+    class Container extends React.Component<any, any> {
+      componentWillReceiveProps(props) {
+        expect(props.data.loading).toBe(false);
+        expect(props.data.allPeople).toEqual(data.allPeople);
+      }
+      render() {
+        return <div>{this.props.children}</div>;
+      }
+    };
+
+    class ContextContainer extends React.Component<any, any> {
+
+      constructor(props) {
+        super(props);
+        this.state = { color: 'purple' };
+      }
+
+      getChildContext() {
+        return { color: this.state.color };
+      }
+
+      componentDidMount() {
+        setTimeout(() => {
+          this.setState({ color: 'green' });
+        }, 50);
+      }
+
+      render() {
+        return <div>{this.props.children}</div>;
+      }
+    }
+
+    (ContextContainer as any).childContextTypes = {
+      color: PropTypes.string,
+    };
+
+    let count = 0;
+    class ChildContextContainer extends React.Component<any, any> {
+      render() {
+        const { color } = (this.context as any);
+        if (count === 0) expect(color).toBe('purple');
+        if (count === 1) {
+          expect(color).toBe('green');
+          done();
+        }
+
+        count++;
+        return <div>{this.props.children}</div>;
+      }
+    }
+
+    (ChildContextContainer as any).contextTypes = {
+      color: PropTypes.string,
+    };
+
+    renderer.create(
+      <ApolloProvider client={client}>
+        <ContextContainer>
+          <Container>
+            <ChildContextContainer />
+          </Container>
+        </ContextContainer>
+      </ApolloProvider>);
+  });
+
+  // meta
+  it('stores the component name in the query metadata', (done) => {
+    const query = gql`query people { allPeople(first: 1) { people { name } } }`;
+    const data = { allPeople: { people: [ { name: 'Luke Skywalker' } ] } };
+    const networkInterface = mockNetworkInterface({ request: { query }, result: { data } });
+    const client = new ApolloClient({ networkInterface, addTypename: false });
+
+    @graphql(query)
+    class Container extends React.Component<any, any> {
+      componentWillReceiveProps(props) {
+        const queries = client.queryManager.getApolloState().queries;
+        const queryIds = Object.keys(queries);
+        expect(queryIds.length).toEqual(1);
+        const query = queries[queryIds[0]];
+        expect(query.metadata).toEqual({
+          reactComponent: {
+            displayName: 'Apollo(Container)',
+          },
+        });
+        done();
+      }
+      render() {
+        return null;
+      }
+    }
+
+    renderer.create(
+      <ApolloProvider client={client}>
+        <Container />
+      </ApolloProvider>
+    );
+  });
+
+  it('uses a custom wrapped component name when \'alias\' is specified', () => {
+    const query = gql`query people { allPeople(first: 1) { people { name } } }`;
+    @graphql(query, {
+      alias: 'withFoo',
+    })
+    class Container extends React.Component<any, any> {
+      render() {
+        return null;
+      }
+    }
+
+    // Not sure why I have to cast Container to any
+    expect((Container as any).displayName).toEqual('withFoo(Container)');
+  });
+
+});
