cb0aeeab2fdfa83b09ec3f7138c9badf71a23e7f
James Baxley <james@meteor.com>
2017-07-20 13:38:04 -0400

    update travis file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,11 +9,6 @@
 # Clear the built output
 rm -rf ./lib
 
-node -e "var package = require('./package.json'); \
-    var fs = require('fs');
-    fs.writeFileSync('./src/version.ts', 'export const version = \"' + package.version + '\"')
-"
-
 # Compile new files
 npm run compile
 
@@ -33,17 +28,20 @@ cd ../
 # The built output as requiring any further transformation.
 node -e "var package = require('./package.json'); \
   delete package.babel; \
+  delete package[\"lint-staged\"]; \
+  delete package.jest; \
+  delete package.bundlesize; \
+  delete package[\"pre-commit\"]; \
   delete package.scripts; \
   delete package.options; \
   package.main = 'react-apollo.umd.js'; \
+  package.browser = 'react-apollo.browser.umd.js'; \
   package.module = 'index.js'; \
   package['jsnext:main'] = 'index.js'; \
   package.typings = 'index.d.ts'; \
   var origVersion = 'local';
   var fs = require('fs'); \
-  fs.writeFileSync('./npm/version.js', 'exports.version = \"' + package.version + '\"'); \
   fs.writeFileSync('./npm/package.json', JSON.stringify(package, null, 2)); \
-  fs.writeFileSync('./src/version.ts', 'export const version = \'' + origVersion + '\';');
   "
 
 
