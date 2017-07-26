33990bc3ea3b8d4d4b6595c7278ae6204525ea04
James Baxley <james@meteor.com>
2017-07-26 17:06:01 -0400

    initial SSR example with meteor

---
+++ examples/ssr/client/main.js
@@ -0,0 +1,27 @@
+import { render } from "react-dom";
+import { onPageLoad } from "meteor/server-render";
+import {
+  ApolloProvider,
+  ApolloClient,
+  createNetworkInterface,
+} from "react-apollo";
+
+import { App } from "/imports/app";
+
+export const start = () => {
+  const client = new ApolloClient({
+    networkInterface: createNetworkInterface({ uri: "http://localhost:3000" }),
+    initialState: { apollo: window.__APOLLO_STATE__ },
+  });
+
+  const WrappedApp = (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+
+  console.log("building react app on the client")
+  render(WrappedApp, document.getElementById("app"));
+};
+
+onPageLoad(start);
