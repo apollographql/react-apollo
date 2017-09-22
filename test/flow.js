5a19ce997efff44ac2ede1f7a4e6df66e9032949
James Baxley <james@meteor.com>
2017-09-22 09:39:25 -0400

    Revert "Upgraded to 0.54.1 Flow (#1002)"
    This reverts commit c2b352151a99ba438c8ca73c088eb109fdeb7d39.

--- test/flow.js
+++ test/flow.js
@@ -10,8 +10,15 @@
 */
 
 // @flow
-import * as React from 'react';
-import { withApollo, compose, gql, graphql } from '../src';
+import { Component } from 'react';
+import {
+  withApollo,
+  compose,
+  gql,
+  graphql,
+  ApolloClient,
+  createNetworkInterface,
+} from '../src';
 import type { OperationComponent, QueryProps, ChildProps } from '../src';
 
 const query = gql`
@@ -27,98 +34,18 @@ const mutation = gql`
 
 type IQuery = {
   foo: string,
-  bar: string,
 };
+
+// common errors
 
 const withData: OperationComponent<IQuery> = graphql(query);
 
-// Test a functional component
-const FunctionalWithData = withData(({ data: { foo } }) => {
-  // $ExpectError string type beeing treated as numerical
+const ComponentWithData = withData(({ data: { foo } }) => {
+  // $ExpectError
   if (foo > 1) return <span />;
 
   return null;
 });
-
-// Test class component, this requieres a stricter definition
-type BasicComponentProps = {
-  data: IQuery & QueryProps,
-  mutate: any, // The mutation is actually required or we get a error at the withData
-};
-class BasicComponent extends React.Component<BasicComponentProps> {
-  render() {
-    const { foo, bar } = this.props.data;
-
-    // $ExpectError string type beeing treated as numerical
-    if (bar > 1) return null;
-
-    // The below works as expected
-    return (
-      <div>
-        {foo.length} string length
-      </div>
-    );
-  }
-}
-const BasicClassWithData = withData(BasicComponent);
-
-// A class component with it's own variable
-type CmplxOwnProps = { faz: string };
-type CmplxComponentProps = {
-  data: IQuery & QueryProps,
-  mutate: any, // The mutation is actually required or we get a error at the withData
-  ...$Exact<CmplxOwnProps>,
-};
-class CmplxComponent extends React.Component<CmplxComponentProps> {
-  render() {
-    const { data: { loading, error, bar, foo }, faz } = this.props;
-    if (loading) return <div>Loading</div>;
-    if (error) return <h1>ERROR</h1>;
-
-    // $ExpectError string type beeing treated as numerical
-    if (bar > 1) return null;
-
-    // The below works as expected
-    return (
-      <div>
-        {foo.length} string length compared to faz {faz.length} length
-      </div>
-    );
-  }
-}
-const withFancyData: OperationComponent<IQuery, CmplxOwnProps> = graphql(query);
-const CmplxWithData = withFancyData(CmplxComponent);
-
-// Same as above but with the Props specified at the end
-// since we don't rely on the ChildProps<P, R> we don't need the mutate: any
-type Cmplx2OwnProps = {| faz: string |}; // We can have exact own props as we don't rely on the TMergedProps
-type Cmplx2ComponentProps = {
-  data: IQuery & QueryProps,
-  ...Cmplx2OwnProps,
-};
-class Cmplx2Component extends React.Component<Cmplx2ComponentProps> {
-  render() {
-    const { data: { loading, error, bar, foo }, faz } = this.props;
-    if (loading) return <div>Loading</div>;
-    if (error) return <h1>ERROR</h1>;
-
-    // $ExpectError string type beeing treated as numerical
-    if (bar > 1) return null;
-
-    // The below works as expected
-    return (
-      <div>
-        {foo.length} string length compared to faz {faz.length} length
-      </div>
-    );
-  }
-}
-const withFancyData2: OperationComponent<
-  IQuery,
-  Cmplx2OwnProps,
-  Cmplx2ComponentProps,
-> = graphql(query);
-const Cmplx2WithData = withFancyData2(Cmplx2Component);
 
 const HERO_QUERY = gql`
   query GetCharacter($episode: Episode!) {
@@ -175,7 +102,7 @@ export default withCharacter(({ loading, hero, error }) => {
   return null;
 });
 
-export class Character extends React.Component<Props> {
+export class Character extends Component {
   render() {
     const { loading, hero, error } = this.props;
     if (loading) return <div>Loading</div>;
@@ -187,7 +114,7 @@ export class Character extends React.Component<Props> {
 const CharacterWithData = withCharacter(Character);
 
 const Manual = withApollo(({ client }) => {
-  // XXX please don't ever actually do this
+  // XXX please don't every actually do this
   client.query({ query: HERO_QUERY });
   return null;
 });
