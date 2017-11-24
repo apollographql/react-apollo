57c995584279d7791151fd26cdd7d682c6cf3ad4
Max Gordon <max@gforge.se>
2017-11-24 22:47:38 +0100

    Updated flow-typed

---
+++ flow-typed/npm/pretty-bytes_v4.x.x.js
@@ -0,0 +1,6 @@
+// flow-typed signature: 97cc406a669d7208a7bd21c6d06d4a48
+// flow-typed version: d7ad169fd2/pretty-bytes_v4.x.x/flow_>=v0.25.x
+
+declare module 'pretty-bytes' {
+  declare module.exports: (num: number) => string;
+}
