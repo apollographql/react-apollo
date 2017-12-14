65d5d4b4a186533b536b4cdfa9be1f50c328e4fd
Kevin Ross <kevin.ross@alienfast.com>
2017-12-13 19:07:40 -0600

    component.test type checks

--- test/react-native/component.test.tsx
+++ test/react-native/component.test.tsx
@@ -5,7 +5,7 @@ import * as renderer from 'react-test-renderer';
 import ApolloClient from 'apollo-client';
 import { InMemoryCache as Cache } from 'apollo-cache-inmemory';
 import gql from 'graphql-tag';
-import { ApolloProvider, graphql } from '../../src';
+import { ApolloProvider, ChildProps, graphql } from '../../src';
 import { mockSingleLink } from '../../src/test-utils';
 import '../setup/toEqualWithoutSymbol';
 
@@ -20,17 +20,25 @@ describe('App', () => {
         }
       }
     `;
-    const data = { allPeople: { people: { name: 'Luke Skywalker' } } };
-    const link = mockSingleLink({ request: { query }, result: { data } });
+
+    interface Data {
+      allPeople: { people: { name: string } };
+    }
+    const link = mockSingleLink({
+      request: { query },
+      result: { data: { allPeople: { people: { name: 'Luke Skywalker' } } } },
+    });
     const client = new ApolloClient({
       link,
       cache: new Cache({ addTypename: false }),
     });
 
-    const ContainerWithData = graphql(query)(({ data }) => {
-      if (data.loading) return <Text>Loading...</Text>;
-      return <Text>{data.allPeople.people.name}</Text>;
-    });
+    const ContainerWithData = graphql(query)(
+      ({ data }: ChildProps<{}, Data>) => {
+        if (data.loading) return <Text>Loading...</Text>;
+        return <Text>{data.allPeople.people.name}</Text>;
+      },
+    );
     const output = renderer.create(
       <ApolloProvider client={client}>
         <ContainerWithData />
@@ -50,18 +58,24 @@ describe('App', () => {
         }
       }
     `;
-    const data = { allPeople: { people: { name: 'Luke Skywalker' } } };
-    const link = mockSingleLink({ request: { query }, result: { data } });
+    interface Data {
+      allPeople: { people: { name: string } };
+    }
+    const data1 = { allPeople: { people: { name: 'Luke Skywalker' } } };
+    const link = mockSingleLink({
+      request: { query },
+      result: { data: data1 },
+    });
     const client = new ApolloClient({
       link,
       cache: new Cache({ addTypename: false }),
     });
 
-    class Container extends React.Component {
+    class Container extends React.Component<ChildProps<{}, Data>> {
       componentWillReceiveProps(props) {
         expect(props.data.loading).toBeFalsy();
         expect(props.data.allPeople.people.name).toEqualWithoutSymbol(
-          data.allPeople.people.name,
+          data1.allPeople.people.name,
         );
         done();
       }
