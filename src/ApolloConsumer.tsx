e89f999a9e579693a09884b4a1cb9e9f5a6e5dcf
Dirk-Jan Rutten <dirkjanrutten@gmail.com>
2017-12-15 20:48:27 +0100

    Wrapped the withApollo component in the ApolloConsumer component.

--- src/ApolloConsumer.tsx
+++ src/ApolloConsumer.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import ApolloClient from 'apollo-client';
-import * as invariant from 'invariant';
+const invariant = require('invariant');
 
-type Props = {
+export interface ApolloConsumerProps {
   render: (client: ApolloClient<any>) => React.ReactElement<any>;
-};
+}
 
-type Context = {
-  client: ApolloClient<any>;
-};
-
-const ApolloConsumer: React.StatelessComponent<Props> = (props, context) => {
+const ApolloConsumer: React.StatelessComponent<ApolloConsumerProps> = (
+  props,
+  context,
+) => {
   invariant(
     !!context.client,
     `Could not find "client" in the context of ApolloConsumer. Wrap the root component in an <ApolloProvider>`,
