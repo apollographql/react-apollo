3506799e09f037e89917f0e966d58e1ffc8d49cc
James Munro <me@jdmunro.net>
2017-12-26 17:30:27 +0000

    Move useRealTimers into the finally rather than before each - better for the test to cleanup after itself.

--- test/react-web/client/graphql/queries/polling.test.tsx
+++ test/react-web/client/graphql/queries/polling.test.tsx
@@ -11,7 +11,6 @@ describe('[queries] polling', () => {
   beforeEach(() => {
     error = console.error;
     console.error = jest.fn(() => {}); // tslint:disable-line
-    jest.useRealTimers();
   });
   afterEach(() => {
     console.error = error;
@@ -69,6 +68,7 @@ describe('[queries] polling', () => {
       done.fail(e);
     } finally {
       (wrapper as any).unmount();
+      jest.useRealTimers();
     }
   });
 
