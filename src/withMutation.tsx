cbfd5fa8e452166b25b87ac34a1e71d4c07ebd5e
Dirk-Jan Rutten <dirkjanrutten@gmail.com>
2018-02-17 21:19:14 +0100

    Added method for determining the props to pass to the wrapped component

--- src/withMutation.tsx
+++ src/withMutation.tsx
@@ -1,21 +1,53 @@
 import * as React from 'react';
 
 import { DocumentNode } from 'graphql';
+// import {
+//   MutationOpts,
+//   OperationOption,
+//   QueryOpts,
+//   GraphqlQueryControls,
+//   MutationFunc,
+//   OptionProps,
+//   DataProps,
+//   MutateProps,
+// } from './types';
+// import { OperationVariables } from './index';
 
 import Mutation from './Mutation';
 
 const withMutation = (
   mutation: DocumentNode,
-  options?: {
-    variables?: Object
-  },
-) => Component => props => (
-  <Mutation mutation={mutation} >
-    {mutate => <Component {...props} mutate={options => {
-      //TODO: Need to update the <Mutation /> component to also take the options as input parameter.
-      return mutate(options);
-    }} />}
-  </Mutation>
-);
+  operationOptions = {},
+) => Component => {
+  return props => {
+    return (
+      <Mutation mutation={mutation}>
+        {mutate => {
+          const clientProps = calculateComponentProps(
+            mutate,
+            props,
+            operationOptions,
+          );
+          
+          return <Component {...props} {...clientProps} />;
+        }}
+      </Mutation>
+    );
+  };
+};
 
 export default withMutation;
+
+const calculateComponentProps = (mutate, ownProps, operationOptions) => {
+  const mapResultToProps = operationOptions.props;
+
+  if (mapResultToProps) {
+    const newResult = {
+      mutate,
+      ownProps,
+    };
+    return mapResultToProps(newResult);
+  }
+
+  return { mutate };
+};
