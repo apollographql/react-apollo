db7ae6802d9c73e47403895502787ff0628ee8ea
Truong Hoang Dung <checkraiser11@gmail.com>
2018-09-08 16:57:54 +0700

    Add renderToStreamWithData

--- src/renderToStringWithData.ts
+++ src/renderToStringWithData.ts
@@ -6,3 +6,7 @@ import { default as getDataFromTree } from './getDataFromTree';
 export function renderToStringWithData(component: ReactElement<any>): Promise<string> {
   return getDataFromTree(component).then(() => ReactDOM.renderToString(component));
 }
+
+export function renderToStreamWithData(component) {
+  return getDataFromTree(component).then(() => renderToNodeStream(component));
+};
