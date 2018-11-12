2048f05053eb9848abd82dd7b8432fb6254994e5
Ben Newman <ben@apollographql.com>
2018-11-12 16:05:41 -0500

    Restore getDataFromTree(rootElement, rootContext) API.
    In react-apollo@2.3.0, the rootContext parameter was replaced by the
    custom renderFunction parameter, which was a breaking change:
    https://github.com/apollographql/react-apollo/pull/2533#discussion_r230251846
    
    This commit restores the previous API, while providing an alternative API
    that is more flexible and more accurately named: getMarkupFromTree.
    
    If you want to specify a custom rendering function, you should now use
    getMarkupFromTree instead of getDataFromTree:
    
      import { getMarkupFromTree } from "react-apollo";
      import { renderToString } from "react-dom/server";
    
      getMarkupFromTree({
        tree: <App/>,
        // optional; defaults to {}
        context: { ... },
        // optional; defaults to renderToStaticMarkup
        renderFunction: renderToString,
      }).then(markup => {
        // Use the markup returned by the final rendering...
      });
    
    Hopefully, since react-apollo@2.3.0 was never assigned the `latest` tag on
    npm, we can push this change without another minor version bump.
    
    cc @evenchange4 @hwillson

--- src/getDataFromTree.ts
+++ src/getDataFromTree.ts
@@ -84,46 +84,60 @@ export class RenderPromises {
   }
 }
 
-class RenderPromisesProvider extends React.Component<{
-  renderPromises: RenderPromises;
-}> {
-  static childContextTypes = {
-    renderPromises: PropTypes.object,
-  };
-
-  getChildContext() {
-    return {
-      renderPromises: this.props.renderPromises,
-    };
-  }
-
-  render() {
-    return this.props.children;
-  }
+export default function getDataFromTree(
+  tree: React.ReactNode,
+  context: { [key: string]: any } = {},
+) {
+  return getMarkupFromTree({
+    tree,
+    context,
+    // If you need to configure this renderFunction, call getMarkupFromTree
+    // directly instead of getDataFromTree.
+    renderFunction: renderToStaticMarkup,
+  });
 }
 
-export default function getDataFromTree(
-  rootElement: React.ReactNode,
+export type GetMarkupFromTreeOptions = {
+  tree: React.ReactNode;
+  context?: { [key: string]: any };
+  renderFunction?: typeof renderToStaticMarkup;
+};
+
+export function getMarkupFromTree({
+  tree,
+  context = {},
   // The rendering function is configurable! We use renderToStaticMarkup as
   // the default, because it's a little less expensive than renderToString,
   // and legacy usage of getDataFromTree ignores the return value anyway.
   renderFunction = renderToStaticMarkup,
-): Promise<string> {
+}: GetMarkupFromTreeOptions): Promise<string> {
   const renderPromises = new RenderPromises();
 
-  function process(): Promise<string> | string {
-    const html = renderFunction(
-      React.createElement(RenderPromisesProvider, {
-        renderPromises,
-        // Always re-render from the rootElement, even though it might seem
-        // better to render the children of the component responsible for the
-        // promise, because it is not possible to reconstruct the full context
-        // of the original rendering (including all unknown context provider
-        // elements) for a subtree of the orginal component tree.
-        children: rootElement,
-      })
-    );
+  class RenderPromisesProvider extends React.Component {
+    static childContextTypes: { [key: string]: any } = {
+      renderPromises: PropTypes.object,
+    };
 
+    getChildContext() {
+      return { ...context, renderPromises };
+    }
+
+    render() {
+      // Always re-render from the rootElement, even though it might seem
+      // better to render the children of the component responsible for the
+      // promise, because it is not possible to reconstruct the full context
+      // of the original rendering (including all unknown context provider
+      // elements) for a subtree of the orginal component tree.
+      return tree;
+    }
+  }
+
+  Object.keys(context).forEach(key => {
+    RenderPromisesProvider.childContextTypes[key] = PropTypes.any;
+  });
+
+  function process(): Promise<string> | string {
+    const html = renderFunction(React.createElement(RenderPromisesProvider));
     return renderPromises.hasPromises()
       ? renderPromises.consumeAndAwaitPromises().then(process)
       : html;
