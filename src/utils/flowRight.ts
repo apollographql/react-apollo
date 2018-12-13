76d005c41a6061ed75b2c49e05d5484f0499f19a
Jovi De Croock <jovi@codifly.be>
2018-12-13 19:54:49 +0100

    refactor: implement requested changes

--- src/utils/flowRight.ts
+++ src/utils/flowRight.ts
@@ -1,10 +1,5 @@
 export function compose(...funcs: Function[]) {
   const functions = funcs.reverse();
-  functions.forEach((fnc, i) => {
-    if (typeof fnc !== 'function') {
-      throw new Error(`In "compose" the argument at index "${i}" is not a function.`);
-    }
-  });
   return function (...args: any[]) {
     const [firstFunction, ...restFunctions] = functions
     let result = firstFunction.apply(null, args);
--- test/client/utils/flowRight.test.tsx
+++ test/client/utils/flowRight.test.tsx
@@ -8,15 +8,4 @@ describe('compose', () => {
     const result = flow(2, 2);
     expect(result).toBe(16);
   });
-
-  it('Should error when an argument is not a function', () => {
-    try {
-      const square = (x: number) => x * x;
-      const flow = compose('x', square);
-      expect(false).toBeTruthy();
-    } catch (e) {
-      expect(true).toBeTruthy();
-      expect(e.message).toEqual('In \"compose\" the argument at index \"1\" is not a function.');
-    }
-  });
 })
