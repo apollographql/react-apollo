0aa1427a844e914c035a4226d753699ffc8f3ab9
Daniel K <FredyC@users.noreply.github.com>
2019-03-16 17:08:41 +0100

    Throw on old React

--- src/useApolloClient.ts
+++ src/useApolloClient.ts
@@ -2,5 +2,8 @@ import React from 'react';
 import { ApolloContext } from './ApolloContext';
 
 export function useApolloClient() {
+  if (!React.useContext) {
+    throw new Error('The useApolloClient needs React version 16.8 or higher');
+  }
   return React.useContext(ApolloContext);
 }
