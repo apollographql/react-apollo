9b24d7567be194c454395365bb5db4fbd7a5caca
Hugh Willson <hugh@octonary.com>
2019-03-21 13:10:29 -0400

    Remove deprecated `walkTree`
    Since there was no way to make `walkTree` behavior consistent with
    modern versions of React, it was deprecated in `react-apollo`
    2.4.1.

--- jest.umd.config.js
+++ jest.umd.config.js
@@ -3,7 +3,6 @@ const { jest } = require('./package.json');
 jest.moduleNameMapper = {
   '\\.\\./src$': '<rootDir>/lib/react-apollo.umd.js',
   '\\.\\./src/test-utils': '<rootDir>/lib/test-utils.js',
-  '\\.\\./src/walkTree': '<rootDir>/lib/walkTree.js',
   // Force other imports to /src/whatever to fail
   '\\.\\./src': '<rootDir>/test/fail-no-entry-point.js',
 };
