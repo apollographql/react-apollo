f8fd0bd2df1d4c46b915fa58fd03040693857a7c
Daniel K <fredy.c@seznam.cz>
2019-03-21 15:57:54 +0100

    Fix typecheck

--- src/useApolloClient.ts
+++ src/useApolloClient.ts
@@ -2,7 +2,7 @@ import React from 'react';
 import { ApolloContext } from './ApolloContext';
 
 export function useApolloClient() {
-  if (!React.useContext) {
+  if (!ApolloContext) {
     throw new Error('The useApolloClient needs React version 16.8 or higher');
   }
   return React.useContext(ApolloContext);
