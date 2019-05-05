86d904664eebe34ac6c0579582377888db40e690
hwillson <hugh@octonary.com>
2019-05-05 18:49:05 -0400

    Remove accidentally added compiled files

--- packages/common/src/index.js
+++
@@ -1,5 +0,0 @@
-export { getApolloContext, resetApolloContext, } from './context/ApolloContext';
-export { ApolloProvider } from './context/ApolloProvider';
-export { ApolloConsumer } from './context/ApolloConsumer';
-export { parser, DocumentType } from './parser/parser';
-//# sourceMappingURL=index.js.map
