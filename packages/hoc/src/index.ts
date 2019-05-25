1888507b63449080339a67f958b20c50b4c90b56
hwillson <hugh@octonary.com>
2019-05-25 06:19:41 -0400

    Re-export SSR utility functions via `@apollo/react-hoc`

--- packages/hoc/src/index.ts
+++ packages/hoc/src/index.ts
@@ -1,12 +1,21 @@
 export { graphql } from './graphql';
+
 export { withQuery } from './query-hoc';
 export { withMutation } from './mutation-hoc';
 export { withSubscription } from './subscription-hoc';
 export { withApollo } from './withApollo';
+
 export {
   ApolloProvider,
   ApolloConsumer,
   getApolloContext,
   resetApolloContext
 } from '@apollo/react-common';
+
+export {
+  getMarkupFromTree,
+  getDataFromTree,
+  renderToStringWithData
+} from '@apollo/react-components';
+
 export * from './types';
