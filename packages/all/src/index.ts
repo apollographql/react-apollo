660f04b0d84eed8009a3773ac4b00fcc1bd43c63
hwillson <hugh@octonary.com>
2019-06-03 13:04:10 -0400

    Add umbrella React Apollo `all` package
    One-stop shop for those looking to include React Apollo Hooks,
    components, and the HOC, in their project (replicating the current
    day functionality of the `react-apollo` package).

---
+++ packages/all/src/index.ts
@@ -0,0 +1,76 @@
+// @apollo/react-common
+export {
+  ApolloContextValue,
+  getApolloContext,
+  resetApolloContext,
+  ApolloProvider,
+  ApolloConsumer,
+  // types
+  OperationVariables,
+  Context,
+  ExecutionResult,
+  BaseQueryOptions,
+  QueryFunctionOptions,
+  ObservableQueryFields,
+  QueryResult,
+  RefetchQueriesFunction,
+  BaseMutationOptions,
+  MutationFunctionOptions,
+  MutationResult,
+  MutationFetchResult,
+  MutationFunction,
+  OnSubscriptionDataOptions,
+  BaseSubscriptionOptions,
+  SubscriptionResult
+} from '@apollo/react-common';
+
+// @apollo/react-components
+export {
+  Query,
+  Mutation,
+  Subscription,
+  // types
+  QueryComponentOptions,
+  MutationComponentOptions,
+  SubscriptionComponentOptions
+} from '@apollo/react-components';
+
+// @apollo/react-hoc
+export {
+  graphql,
+  withQuery,
+  withMutation,
+  withSubscription,
+  withApollo,
+  // types
+  QueryControls,
+  DataValue,
+  DataProps,
+  MutateProps,
+  ChildProps,
+  OptionProps,
+  OperationOption
+} from '@apollo/react-hoc';
+
+// @apollo/react-hooks
+export {
+  useQuery,
+  useMutation,
+  useSubscription,
+  useApolloClient,
+  getMarkupFromTree,
+  getDataFromTree,
+  renderToStringWithData,
+  // types
+  CommonOptions,
+  QueryOptions,
+  QueryHookOptions,
+  QueryPreviousData,
+  QueryCurrentObservable,
+  MutationHookOptions,
+  MutationOptions,
+  MutationTuple,
+  SubscriptionHookOptions,
+  SubscriptionOptions,
+  SubscriptionCurrentObservable
+} from '@apollo/react-hooks';
