bd71cb6266be410d88995629af603735ae83e33c
hwillson <hugh@octonary.com>
2019-06-11 13:07:56 -0400

    Compile ESM bundle to CJS using Babel instead of Rollup
    Follows in the footsteps of
    https://github.com/apollographql/apollo-client/pull/4911.

--- packages/testing/config/rollup.config.js
+++ packages/testing/config/rollup.config.js
@@ -1,72 +1,8 @@
-import node from 'rollup-plugin-node-resolve';
-import typescript from 'typescript';
-import typescriptPlugin from 'rollup-plugin-typescript2';
+import { rollup } from '../../../config/rollup.config';
 
-function onwarn(message) {
-  const suppressed = ['UNRESOLVED_IMPORT', 'THIS_IS_UNDEFINED'];
-
-  if (!suppressed.find(code => message.code === code)) {
-    return console.warn(message.message);
+export default rollup({
+  name: 'testing',
+  extraGlobals: {
+    optimism: 'wrap'
   }
-}
-
-const globals = {
-  'fast-json-stable-stringify': 'stringify',
-  'zen-observable': 'zenObservable',
-  optimism: 'wrap',
-  react: 'React',
-  'prop-types': 'PropTypes',
-  'hoist-non-react-statics': 'hoistNonReactStatics',
-  '@apollo/react-common': 'apolloReactCommon',
-  tslib: 'tslib',
-};
-
-function external(id) {
-  return Object.prototype.hasOwnProperty.call(globals, id);
-}
-
-const SRC_DIR = './src';
-const LIB_DIR = './lib';
-
-export default [
-  {
-    input: `${SRC_DIR}/index.ts`,
-    output: {
-      file: `${LIB_DIR}/react-testing.esm.js`,
-      format: 'esm',
-      sourcemap: true,
-      globals,
-    },
-    external,
-    plugins: [
-      node({ mainFields: ['module'] }),
-      typescriptPlugin({
-        typescript,
-        tsconfig: './config/tsconfig.json',
-      }),
-    ],
-    onwarn,
-  },
-  {
-    input: `${LIB_DIR}/react-testing.esm.js`,
-    output: {
-      file: `${LIB_DIR}/react-testing.cjs.js`,
-      format: 'cjs',
-      name: 'react-testing',
-      globals,
-    },
-    external,
-    onwarn,
-  },
-  {
-    input: `${LIB_DIR}/react-testing.esm.js`,
-    output: {
-      file: `${LIB_DIR}/react-testing.umd.js`,
-      format: 'umd',
-      name: 'react-testing',
-      globals,
-    },
-    external,
-    onwarn,
-  },
-];
+});
