28acdb6c0feab15a160e9448cbfe8a7ace235c31
hwillson <hugh@octonary.com>
2019-06-25 12:23:13 -0400

    Initialize data as {} when using useQuery with SSR
    This is to line up with the `useQuery` initial client side `data`
    value of `{}`. This changed when we moved from using the
    deprecated `ObservableQuery.currentResult()` method to the newer
    `ObservableQuery.getCurrentResult()` method (which doesn't
    automatically convert the initial `undefined` `data` value to
    `{}`).
    
    Fixes #3143.

--- packages/hooks/src/__tests__/useQuery.test.tsx
+++ packages/hooks/src/__tests__/useQuery.test.tsx
@@ -4,54 +4,157 @@ import gql from 'graphql-tag';
 import { MockedProvider } from '@apollo/react-testing';
 import { render, cleanup } from '@testing-library/react';
 import { useQuery } from '@apollo/react-hooks';
+import { renderToStringWithData } from '@apollo/react-ssr';
 
 describe('useQuery Hook', () => {
   afterEach(cleanup);
 
-  it('should handle a simple query properly', done => {
-    const query: DocumentNode = gql`
-      query {
-        cars {
-          make
-          model
-          vin
+  describe('General use', () => {
+    it('should handle a simple query properly', done => {
+      const query: DocumentNode = gql`
+        query {
+          cars {
+            make
+            model
+            vin
+          }
         }
-      }
-    `;
+      `;
 
-    const resultData = {
-      cars: [
+      const resultData = {
+        cars: [
+          {
+            make: 'Audi',
+            model: 'RS8',
+            vin: 'DOLLADOLLABILL',
+            __typename: 'Car'
+          }
+        ]
+      };
+
+      const mocks = [
         {
-          make: 'Audi',
-          model: 'RS8',
-          vin: 'DOLLADOLLABILL',
-          __typename: 'Car'
+          request: {
+            query
+          },
+          result: { data: resultData }
         }
-      ]
-    };
+      ];
 
-    const mocks = [
-      {
-        request: {
-          query
-        },
-        result: { data: resultData }
-      }
-    ];
+      const Component = () => {
+        const { data, loading } = useQuery(query);
+        if (!loading) {
+          expect(data).toEqual(resultData);
+          done();
+        }
+        return null;
+      };
 
-    const Component = () => {
-      const { data, loading } = useQuery(query);
-      if (!loading) {
-        expect(data).toEqual(resultData);
-        done();
-      }
-      return null;
-    };
+      render(
+        <MockedProvider mocks={mocks}>
+          <Component />
+        </MockedProvider>
+      );
+    });
+  });
 
-    render(
-      <MockedProvider mocks={mocks}>
-        <Component />
-      </MockedProvider>
-    );
+  describe('SSR', () => {
+    it('should support SSR', () => {
+      const query: DocumentNode = gql`
+        query {
+          cars {
+            make
+            model
+            vin
+          }
+        }
+      `;
+
+      const resultData = {
+        cars: [
+          {
+            make: 'Audi',
+            model: 'RS8',
+            vin: 'DOLLADOLLABILL',
+            __typename: 'Car'
+          }
+        ]
+      };
+
+      const mocks = [
+        {
+          request: {
+            query
+          },
+          result: { data: resultData }
+        }
+      ];
+
+      const Component = () => {
+        const { data, loading } = useQuery(query);
+        if (!loading) {
+          expect(data).toEqual(resultData);
+          const { make, model, vin } = data.cars[0];
+          return (
+            <div>
+              {make}, {model}, {vin}
+            </div>
+          );
+        }
+        return null;
+      };
+
+      const app = (
+        <MockedProvider mocks={mocks}>
+          <Component />
+        </MockedProvider>
+      );
+
+      return renderToStringWithData(app).then(markup => {
+        expect(markup).toMatch(/Audi/);
+      });
+    });
+
+    it('should initialize data as an empty object when loading', () => {
+      const query: DocumentNode = gql`
+        query {
+          foo {
+            bar
+          }
+        }
+      `;
+
+      const resultData = {
+        foo: {
+          __typename: 'Foo',
+          bar: 'baz'
+        }
+      };
+
+      const mocks = [
+        {
+          request: {
+            query
+          },
+          result: { data: resultData }
+        }
+      ];
+
+      const Component = () => {
+        const { data, loading } = useQuery(query);
+        if (loading) {
+          expect(data).toEqual({});
+        }
+        return null;
+      };
+
+      const app = (
+        <MockedProvider mocks={mocks}>
+          <Component />
+        </MockedProvider>
+      );
+
+      return renderToStringWithData(app);
+    });
   });
 });
