bca93445b8f9629dc37097635fc55d6ee824d2f2
hwillson <hugh@octonary.com>
2019-06-28 07:53:27 -0400

    Additional test to verify `useMutation` can be used in an effect

--- packages/hooks/src/__tests__/useMutation.test.tsx
+++ packages/hooks/src/__tests__/useMutation.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { DocumentNode } from 'graphql';
 import gql from 'graphql-tag';
 import { MockedProvider } from '@apollo/react-testing';
@@ -63,6 +63,80 @@ describe('useMutation Hook', () => {
         default:
       }
       renderCount += 1;
+      return null;
+    };
+
+    render(
+      <MockedProvider mocks={mocks}>
+        <Component />
+      </MockedProvider>
+    );
+  });
+
+  it('should be able to call mutations as an effect', done => {
+    const mutation: DocumentNode = gql`
+      mutation createTodo($description: String!) {
+        createTodo(description: $description) {
+          id
+          description
+          priority
+        }
+      }
+    `;
+
+    const variables = {
+      description: 'Get milk!'
+    };
+
+    const resultData = {
+      createTodo: {
+        id: 1,
+        description: 'Get milk!',
+        priority: 'High',
+        __typename: 'Todo'
+      }
+    };
+
+    const mocks = [
+      {
+        request: {
+          query: mutation,
+          variables
+        },
+        result: { data: resultData }
+      }
+    ];
+
+    let renderCount = 0;
+    const useCreateTodo = () => {
+      const [createTodo, { loading, data }] = useMutation(mutation);
+
+      useEffect(() => {
+        createTodo({ variables });
+      }, [variables]);
+
+      switch (renderCount) {
+        case 0:
+          expect(loading).toBeFalsy();
+          expect(data).toBeUndefined();
+          break;
+        case 1:
+          expect(loading).toBeTruthy();
+          expect(data).toBeUndefined();
+          break;
+        case 2:
+          expect(loading).toBeFalsy();
+          expect(data).toEqual(resultData);
+          done();
+          break;
+        default:
+      }
+      renderCount += 1;
+      return null;
+    };
+
+    const Component = () => {
+      useCreateTodo();
       return null;
     };
 
