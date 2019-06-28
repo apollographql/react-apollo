c73f287b9aee545801acc09f1579a8fabf17b05e
hwillson <hugh@octonary.com>
2019-06-28 18:22:32 -0400

    Replace `console.warn` with `invariant.warn`

--- packages/hooks/src/useMutation.ts
+++ packages/hooks/src/useMutation.ts
@@ -1,6 +1,7 @@
 import { useContext, useState, useRef, useEffect } from 'react';
 import { getApolloContext, OperationVariables } from '@apollo/react-common';
 import { DocumentNode } from 'graphql';
+import { invariant } from 'ts-invariant';
 
 import { MutationHookOptions, MutationTuple } from './types';
 import { MutationData } from './data/MutationData';
@@ -15,7 +16,7 @@ export function useMutation<TData = any, TVariables = OperationVariables>(
   // breaking `useMutation` API change during the RA 3 beta process. It will
   // be removed before the official launch of RA 3.
   if (showApiChangeWarning) {
-    console.warn(
+    invariant.warn(
       'PLEASE NOTE: The `useMutation` API has changed; the tuple based ' +
         'return value now has the mutation result in the first position, ' +
         'e.g. [Mutation Result, Mutation Function]. See ' +
