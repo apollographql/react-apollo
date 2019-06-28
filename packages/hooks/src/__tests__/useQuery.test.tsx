86866259d9226a3a406020d8fe2e7682800cc4b6
hwillson <hugh@octonary.com>
2019-06-28 09:12:14 -0400

    Add `useQuery` start/stop polling test

--- packages/hooks/src/__tests__/useQuery.test.tsx
+++ packages/hooks/src/__tests__/useQuery.test.tsx
@@ -54,4 +54,73 @@ describe('useQuery Hook', () => {
       </MockedProvider>
     );
   });
+
+  it('should support polling', done => {
+    const query: DocumentNode = gql`
+      query {
+        cars {
+          make
+          model
+          vin
+        }
+      }
+    `;
+
+    const resultData = {
+      cars: [
+        {
+          make: 'Audi',
+          model: 'RS8',
+          vin: 'DOLLADOLLABILL',
+          __typename: 'Car'
+        }
+      ]
+    };
+
+    const mocks = [
+      {
+        request: {
+          query
+        },
+        result: { data: resultData }
+      }
+    ];
+
+    let renderCount = 0;
+    const Component = () => {
+      let { data, loading, stopPolling } = useQuery(query, {
+        pollInterval: 10
+      });
+      switch (renderCount) {
+        case 0:
+          expect(loading).toBeTruthy();
+          break;
+        case 1:
+          expect(loading).toBeFalsy();
+          expect(data).toEqual(resultData);
+          break;
+        case 2:
+          expect(loading).toBeFalsy();
+          expect(data).toEqual(resultData);
+          stopPolling();
+          setTimeout(() => {
+            done();
+          }, 10);
+          break;
+        case 3:
+          done.fail('Uh oh - we should have stopped polling!');
+          break;
+        default:
+        // Do nothing
+      }
+      renderCount += 1;
+      return null;
+    };
+
+    render(
+      <MockedProvider mocks={mocks}>
+        <Component />
+      </MockedProvider>
+    );
+  });
 });
