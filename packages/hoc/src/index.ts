70936bbc9d742ff3680ae3ad8c30c4403f6ae40c
hwillson <hugh@octonary.com>
2019-08-04 21:38:48 -0400

    Move `WithApolloClient` into the HOC `types.ts` file

--- packages/hoc/src/index.ts
+++ packages/hoc/src/index.ts
@@ -3,7 +3,7 @@ export { graphql } from './graphql';
 export { withQuery } from './query-hoc';
 export { withMutation } from './mutation-hoc';
 export { withSubscription } from './subscription-hoc';
-export { withApollo, WithApolloClient } from './withApollo';
+export { withApollo } from './withApollo';
 
 export {
   ApolloProvider,
